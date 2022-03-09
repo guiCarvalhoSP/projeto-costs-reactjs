7b6ddfcfcb742f24b56d471649c500005254d9f8
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2022-03-09 16:28:43 -0300

    Novas funções e adição de banco

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,9 +1,38 @@
+import { useNatigate } from 'react-router-dom'
+
+import ProjectForm from '../project/ProjectForm';
+import styles from './NewProject.module.css'
+
 function NewProject(){
 
+    const navigate = useNatigate()
+
+    function createPost(project) {
+        
+        //Initialize cost and services
+        project.cost = 0;
+        project.services =[];
+
+        fetch('http://localhost:5000/projects', {
+            method: 'POST',
+            Headers: {
+                'Content-type': 'application/json',
+            },
+        })
+        .then(resp => resp.json)
+        .then((data) => {
+            // Redirect
+            navigate('/projects', {message: 'Projeto criado com sucesso!'})
+        })
+        .cathch(err => console.log(err))
+    }
+
     return(
-        <h1>
-            NewProject
-        </h1>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviço</p>
+            <ProjectForm handleSubmit={createPost} btnText="Criar projeto"/>
+        </div>
     )
 }
 
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,7 +1,57 @@
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import Message from '../../layout/Message';
+import Container from '../../layout/Container';
+import LinkButton from '../../layout/partials/LinkButton';
+import ProjectCard from '../project/ProjectCard';
+
+import styles from './Project.module.css';
+
 function Project(){
+
+    const[projects, setProjects] = useState([]);
+
+    const location = useLocation();
+    let message = '';
+    if(location.state){
+        message = location.state.message;
+    }
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setProjects(data)
+        })
+        .catch((err) => console.log(err));
+    }, []);
+
     return (
-        <div>
-            
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+            <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+                {message && <Message type="success" msg={message} />}
+            </div>
+           
+            <Container customClass="start">
+                {projects.length > 0 && 
+                projects.map((project) => (
+                    <ProjectCard 
+                        name={project.name}
+                        id={project.id}
+                        budget={project.budget}
+                        category={project.category.name}
+                        key={project.id}
+                    />
+                ))}
+            </Container>
         </div>
     )
 }
